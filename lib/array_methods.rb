f1c379ea83e87b83a813726d68a04921d3ce3843
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-20 18:00:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,6 +15,7 @@ def find_max_value(array)
   while array[counter] do
     if array[counter] > max then
       max = array[counter]
+      counter++
     end
   end
   max
@@ -27,6 +28,7 @@ def find_min_value(array)
   while array[counter] do
     if array[counter] > max then
       min = array[counter]
+      counter++
     end
   end
   min
